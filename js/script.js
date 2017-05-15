692c634aaa029de0c7a604062faa732cd1af1710
William Lotosky <wlotosky@gmail.com>
2017-05-15 18:04:57 -0400

    edited the script file

--- js/script.js
+++ js/script.js
@@ -29,37 +29,71 @@ var houses = {
 	},
 }
 var questionCounter = 0;
-var redCounter = 0;
-var greenCounter = 0;
-var blueCounter = 0;
-var yellowCounter = 0;
-var finalArray =[redCounter, greenCounter, blueCounter, yellowCounter];
+var redCounter = 4;
+var greenCounter = 5;
+var blueCounter = 6;
+var yellowCounter = 7;
+
+var houses = [
+	{
+	house: 'gryffindor',
+	counter: redCounter
+	},
+	{
+	house: 'ravenclaw',
+	counter: blueCounter
+	},
+	{
+	house: 'slytherin',
+	counter: greenCounter
+	},
+	{
+	house: 'hufflepuff',
+	counter: yellowCounter
+	}
+]
 
 $(document).ready(function(){
 	$('.house-next').on('click', function(){
-		questionCounter++;
+		if (questionCounter < 4) {
+			questionCounter++;
 
-		$('.questions h2').text(houses['question' + questionCounter].question);
-		$('label[for = "answer1"]').text(houses['question' + questionCounter].red);
-		$('label[for = "answer2"]').text(houses['question' + questionCounter].green);
-		$('label[for = "answer3"]').text(houses['question' + questionCounter].blue);
-		$('label[for = "answer4"]').text(houses['question' + questionCounter].yellow);
-		
-		var userAnswer = $('input:checked').val();
-		if(userAnswer === 'red')
-			redCounter++;
-		else if(userAnswer === 'blue')
-			blueCounter++;
-		else if(userAnswer === 'green')
-			greenCounter++;
-		else if(userAnswer === 'yellow')
-			yellowCounter++;
+			$('.questions h2').text(houses['question' + questionCounter].question);
+			$('label[for = "answer1"]').text(houses['question' + questionCounter].red);
+			$('label[for = "answer2"]').text(houses['question' + questionCounter].green);
+			$('label[for = "answer3"]').text(houses['question' + questionCounter].blue);
+			$('label[for = "answer4"]').text(houses['question' + questionCounter].yellow);
+			
+			var userAnswer = $('input:checked').val();
+			if(userAnswer === 'red')
+				redCounter++;
+			else if(userAnswer === 'blue')
+				blueCounter++;
+			else if(userAnswer === 'green')
+				greenCounter++;
+			else if(userAnswer === 'yellow')
+				yellowCounter++;
 
 
-		console.log(userAnswer);
-		console.log(questionCounter);
+			console.log(userAnswer, questionCounter);
+			for (var i = 0; i < 4; i++) {
+				finalArray.push({
+
+				});
+			}
+		}
+		else {
+			$('body').html(`<h1>You're in Gryffindor!</h1>`)
+		}
 		
 	});
+
+	houses.sort(function(a,b) {
+	   return b.counter - a.counter
+	});
+
+	console.log(houses[0])
+
 });
 
 
